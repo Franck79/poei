bf1502a890d84f07ca4dcd3d69fcd3e4bf533f25
Franck Moeuf <fmoeuf@gmail.com>
2019-01-03 14:06:21 +0100

    Modif deploy-prod.sh

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
+
+#Début maintenance
 drush sset system.maintenance_node 1
 echo Maintenance mode enable.
 drush cr
+
+#Pull master origin
 git pull origin master
 echo Pull Master done.
+
+#Installation
 composer install --no-dev
 echo Installation/Update Drupal Core.
 drush cr
+
+#Update
 drush updb
 echo Update modules.
 drush cr
+
+#Export config prod
 drush csex prod -y
 echo Exports config Prod.
+
+#Import new config
 drush cim -y
 echo Import config ok.
 drush cr
+
+#Ajout des config de prod
+git add config/prod
+git commit -m 'Ajout des config de prod.'
+
+
+#Fin maintenance
 drush sset system.maintenance_node 0
 drush cr
 echo Site is online.
